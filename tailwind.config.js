71852b9ea9a7f38c29277143b049fe7a8419e1de
LUIS ALEJANDRO GOMEZ CASTANO <LAGOMCAS@davivienda.loc>
2022-10-13 16:18:48 -0500

    Para que los baje oso

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'wave-pattern': 'url(../src/img/wave-white.png)',
+        'wave-pattern-dots': 'url(../src/img/pattern-white-dots.png)',
+        'pattern-characters-red': 'url(../src/img/pattern-characters-red.png)',
+        'character-mario': 'url(../src/img/character-l.png)',
+        'wave-pink': 'url(../src/img/wave-pink.png)',
+        'yellow-dots': 'url(../src/img/pattern-yellow-dots.png)',
+
+      })
+    },
+  },
+  plugins: [],
+}
